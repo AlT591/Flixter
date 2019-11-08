65ba7fa8f86f4bcb89b5a6bb2038238a4f32b8af
Al Trujillo <Trujillo0525@gmail.com>
2019-11-08 23:51:11 +0000

    created devise gem added sign up and log in pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # lesson 11 devise gem install instructions
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
